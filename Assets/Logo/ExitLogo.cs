2eaa14dc8a5db1cd6b29ef79c10929f7d8e81fc9
Luiz Augusto <luiz.m.santiago@hotmail.com>
2021-01-20 13:25:39 -0300

    Não sei

---
+++ Assets/Logo/ExitLogo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLogo : MonoBehaviour
+{
+    public string newMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(NewFase());
+    }
+    IEnumerator NewFase()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(newMap);
+    }
+}
